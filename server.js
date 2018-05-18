43e9a8a303e913f8ae58dbf62c8045d589931db5
Pek Ratanak <r.pek@gl-f.com>
2018-05-18 16:42:15 +0700

    Socket working

--- server.js
+++ server.js
@@ -2,8 +2,29 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname+'/public'));
+
+io.on('connection', function(socket){
+    console.log('User connected via socket.io in server!!!');
+
+    //server listen
+    socket.on('message', function(message){
+        console.log('Message Received: '+message.text);
+
+        //server broadcast back to client
+        socket.broadcast.emit('message', message);
+    });
+
+    //server broadcast back to client
+    socket.emit('message', {
+        text:'Welcome to the chat applicaiton!'
+    })
+});
+
+
+
 http.listen(PORT, function(){
     console.log('Server Started!!!');
 });
